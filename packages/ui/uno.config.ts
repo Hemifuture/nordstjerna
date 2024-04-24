31bc09e3ad9883fb3f0eff6f931009da28fab9b9
Du Yizhuo <dyzdyz010@gmail.com>
2024-04-24 19:30:41 +0800

    fix: Add uno preset

--- packages/ui/uno.config.ts
+++ packages/ui/uno.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from "unocss";
 import transformerDirectives from "@unocss/transformer-directives";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
+import presetUno from "@unocss/preset-uno";
 import { presetIcons } from "@unocss/preset-icons";
 
 export default defineConfig({
   transformers: [transformerDirectives(), transformerVariantGroup()],
   // ...UnoCSS options
-  presets: [presetIcons()],
+  presets: [presetUno(), presetIcons()],
 });
