62253e7cbcfa400575cb614a53396f4436f17940
Du Yizhuo <dyzdyz010@gmail.com>
2024-07-31 16:47:56 +0800

    update: 改写部分样式

--- playground/src/views/style/index.tsx
+++ playground/src/views/style/index.tsx
@@ -10,6 +10,7 @@ const StyleView = () => {
         <div class="ns-glass ns-level-medium rounded-md p4">asdf</div>
         <div class="ns-glass ns-level-high rounded-md p4">asdf</div>
         <NButton level="low">Button</NButton>
+        <a href="#" class="link" style="font-size: 4rem;">Link</a>
       </div>
     </>
   );
