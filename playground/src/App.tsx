5e8d7123264cb186ee56693d03370d05a06380ec
dyzdyz010 <dyzdyz010@gmail.com>
2025-04-09 21:20:33 +0800

    feat(style): Add input style. Add danger style for button and input class

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -3,14 +3,27 @@ import { NButton } from '@nordstjerna/ui';
 function App() {
   return (
     <>
-      <div class='card'>
-        <NButton className='button'>你好</NButton>
-        <NButton className='button'>No Level</NButton>
-        <NButton className='button level-low'>Low Level</NButton>
-        <NButton className='button level-medium'>Medium Level</NButton>
-        <NButton className='button level-high'>High Level</NButton>
-        <NButton className='button success level-low'>Success</NButton>
-        <NButton className='button warning level-high'>Warning</NButton>
+      <div class='card level-low buttons'>
+        <h1>Buttons</h1>
+        <div class='button-group'>
+          <NButton className='button'>你好</NButton>
+          <NButton className='button'>No Level</NButton>
+          <NButton className='button level-low'>Low Level</NButton>
+          <NButton className='button level-medium'>Medium Level</NButton>
+          <NButton className='button level-high'>High Level</NButton>
+          <NButton className='button success level-low'>Success</NButton>
+          <NButton className='button warning level-high'>Warning</NButton>
+          <NButton className='button danger level-high'>Danger</NButton>
+        </div>
+      </div>
+      <div class='card level-low inputs'>
+        <h1>Inputs</h1>
+        <div class='input-group'>
+          <input class='input' />
+          <input class='input success level-low' />
+          <input class='input warning level-medium' />
+          <input class='input danger level-high' />
+        </div>
       </div>
     </>
   );
